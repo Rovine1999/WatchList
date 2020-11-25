d14ed323da4cb11283f8a0f3d39251d7b2ea73d7
Rovine1999 <rovinewanjala99@gmail.com>
2020-11-26 00:33:00 +0300

    add css

--- app/views.py
+++ app/views.py
@@ -1,6 +1,7 @@
 from flask import render_template
 from app import app
-from .request import get_movies
+from .request import get_movies, get_movie
+
 # Views
 @app.route('/')
 def index():
@@ -18,11 +19,14 @@ def index():
 
 
 
-@app.route('/movie/<int:movie_id>')
-def movie(movie_id):
+@app.route('/movie/<int:id>')
+def movie(id):
 
     '''
     View movie page function that returns the movie details page and its data
     '''
-    return render_template('movie.html',id = movie_id)
+    movie = get_movie(id)
+    title = f'{movie.title}'
+
+    return render_template('movie.html', title = title, movie = movie)
 
